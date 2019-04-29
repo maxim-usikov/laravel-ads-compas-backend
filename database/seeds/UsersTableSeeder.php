528102a878e66cf0dcdcd3712ab3de2c935d0bd1
Maxim Usikov <mu.usikov@gmail.com>
2019-04-29 21:15:28 +0300

    build: add users seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class, 50)->create();
+    }
+}
