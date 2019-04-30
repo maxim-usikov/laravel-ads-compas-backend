7cc1a06d2f259a71932ebff59addf60602da72c8
Maxim Usikov <mu.usikov@gmail.com>
2019-04-30 10:23:55 +0300

    feat: add seeders to database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UsersTableSeeder::class,
+            AuthorsTableSeeder::class,
+            BooksTableSeeder::class,
+            AuthorBookTableSeeder::class,
         ]);
     }
 }
