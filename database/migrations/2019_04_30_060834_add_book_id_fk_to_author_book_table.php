ec95483e9911fb105c00eab22d28c7635652cb68
Maxim Usikov <mu.usikov@gmail.com>
2019-04-30 09:13:36 +0300

    feat: add book_id fk to author_book

---
+++ database/migrations/2019_04_30_060834_add_book_id_fk_to_author_book_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBookIdFkToAuthorBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('author_book', function (Blueprint $table) {
+            $table->foreign('book_id')
+                  ->references('id')
+                  ->on('books')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('author_book', function (Blueprint $table) {
+            $table->dropForeign(['book_id']);
+        });
+    }
+}
