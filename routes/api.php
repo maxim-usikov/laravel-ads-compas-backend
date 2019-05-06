3251e0fdde04efa01d64014930dfe03347cf7f62
Maxim Usikov <mu.usikov@gmail.com>
2019-05-06 13:09:26 +0300

    fix: re-name `api/user` to `api/me` route

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function () {
+    Route::get('/me', function (Request $request) {
+        return $request->user();
+    });
 });
 
 Route::apiResource('books', 'API\BookController')->only(['index']);
