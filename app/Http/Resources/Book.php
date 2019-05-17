50069b610c7d839fae71efd67289867acc16f67b
Maxim Usikov <mu.usikov@gmail.com>
2019-05-17 10:14:13 +0300

    chore: cleanup

--- app/Http/Resources/Book.php
+++ app/Http/Resources/Book.php
@@ -7,13 +7,6 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class Book extends JsonResource
 {
     /**
-     * Indicates if the resource's collection keys should be preserved.
-     *
-     * @var bool
-     */
-    // public $preserveKeys = true;
-
-    /**
      * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request  $request
